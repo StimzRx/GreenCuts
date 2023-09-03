51031032fa3ac989db0ecee280c53306afce6887
VenomCodeDev <103589496+VenomCodeDev@users.noreply.github.com>
2023-09-03 14:47:54 -0500

    Added ServerAPI reference, removed shadowed jars, fixed config setup

--- src/main/java/dev/venomcode/greencuts/GreenCuts.java
+++ src/main/java/dev/venomcode/greencuts/GreenCuts.java
@@ -1,7 +1,15 @@
 package dev.venomcode.greencuts;
 
+import dev.venomcode.serverapi.api.ServerAPI;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.tinyremapper.extension.mixin.common.Logger;
+import org.spongepowered.configurate.CommentedConfigurationNode;
+import org.spongepowered.configurate.ConfigurateException;
+import org.spongepowered.configurate.hocon.HoconConfigurationLoader;
+
+import java.nio.file.Path;
+
+import static com.mojang.text2speech.Narrator.LOGGER;
 
 public class GreenCuts implements ModInitializer {
     public static final String MODID = "greencuts";
@@ -16,12 +24,36 @@ public class GreenCuts implements ModInitializer {
         config = new GreenCutsConfig();
     }
 
-    public static GreenCutsConfig getConfig()
-    {
-        return config;
+
+    public static GreenCutsConfig getConfig() {
+        if(_configCached != null)
+            return _configCached;
+
+        try {
+            CommentedConfigurationNode node = configLoader.load();
+
+            _configCached = node.get(GreenCutsConfig.class);
+        }
+        catch (ConfigurateException ex) {
+            LOGGER.error(ServerAPI.Logger.Error("[ERROR]Failed to load greencuts config."));
+        }
+
+        return _configCached;
     }
-    public static void debugLog(String msg)
-    {
-        logger.info( "[" + MODID + "]" + msg);
+
+    public static void saveConfig() {
+        CommentedConfigurationNode node = CommentedConfigurationNode.root();
+        try {
+            node.set(GreenCutsConfig.class, _configCached);
+            configLoader.save(node);
+        }
+        catch (ConfigurateException ex) {
+            LOGGER.error(ServerAPI.Logger.Error("[ERROR]Failed to save greencuts config."));
+        }
     }
+
+    private static final HoconConfigurationLoader configLoader = HoconConfigurationLoader.builder()
+            .path(Path.of(ServerAPI.CONFIG_PATH + "greencuts.conf"))
+            .build();
+    private static GreenCutsConfig _configCached = null;
 }
